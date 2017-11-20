7b1fdc56055bbc1e87b8d1289fb7ccbd764255a3
LikeaNachozBR <30394349+LikeaNachozBR@users.noreply.github.com>
2017-11-20 02:21:20 -0300

    Create Fliper.cpp

---
+++ solucoes/Fliper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int x; int y;
+ cin >> x >> y;
+ if (x==0) {
+  cout << "C" << "\n";
+ } else {
+  if (y == 0) {
+   cout << "B" << "\n";
+  } else {
+   cout << "A" << "\n";
+  }
+ }
+}
