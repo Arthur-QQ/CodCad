ff91c02cf9ab7c5a185f5e47a29e3026f5d62474
LikeaNachozBR <30394349+LikeaNachozBR@users.noreply.github.com>
2017-11-20 02:15:14 -0300

    Create Tomadas.cpp

---
+++ solucoes/Tomadas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t1; int t2;
+    int t3; int t4;
+    cin>>t1>>t2>>t3>>t4;
+    int soma;
+    soma = t1 + t2 + t3 + t4;
+    cout<<soma-3<<"\n";
+    return 0;
+}
