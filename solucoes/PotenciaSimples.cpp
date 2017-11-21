a4329b589641392aac9a863dc89ffbc2cb00c365
LikeaNachozBR <30394349+LikeaNachozBR@users.noreply.github.com>
2017-11-21 00:26:55 -0300

    Create PotenciaSimples.cpp

---
+++ solucoes/PotenciaSimples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(4);
+    cout.setf(ios::fixed);
+    cout << pow(a, b) << endl;
+    return 0;
+}
