c8f4a5812234f71cead31fb0259f79338de149ab
Arthur-QQ <30394349+Arthur-QQ@users.noreply.github.com>
2018-02-18 14:02:49 -0300

    Update AlarmeDespertador.cpp

--- solucoes/AlarmeDespertador.cpp
+++ solucoes/AlarmeDespertador.cpp
@@ -1,19 +1,21 @@
-
 #include <iostream>
 using namespace std;
-int main() {
-int h1, m1, h2, m2 = -1;
-while (h1 != 0 and m1 != 0 and h2 != 0 and m2 != 0) {
-cin>>h1>>m1>>h2>>m2;
-if (h2 < h1 or (h1 == h2 and m2 < m1)) {
-h2 += 24;
-}
-int t1 = h1*60+m1;
-int t2 = h2*60+m2;
-if(!(h1 == 0 and m1 == 0 and h2 == 0 and m2 == 0)) {
-cout<<t2-t1<<"\n";
-}
-}
 
-return 0;
+int main() {
+    int h1, m1, h2, m2 = 1;
+    while (true) {
+        cin>>h1>>m1>>h2>>m2;
+        if (h1 == 0 and m1 == 0 and h2 == 0 and m2 == 0) {
+            break;
+        }
+
+        int o;
+        if (h1 > h2 or (h2 == h1 and m1 > m2)) {
+            h2 += 24;
+        }
+        o = (h2 - h1) * 60 + m2 - m1;
+        cout<<o<<endl;
+    }
+
+    return 0;
 }
